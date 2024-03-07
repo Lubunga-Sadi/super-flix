550ed1d64cb00625f97f668cdccabaa08801eec6
Lubunga <innocentsadi6@gmail.com>
2024-03-07 09:38:30 -0600

    first commit

---
+++ utils.js
@@ -0,0 +1,16 @@
+'use strict';
+
+
+export function getElement(selector, scope = document) {
+  return scope.getElementById(selector);
+}
+
+// Select HTML element
+export function select(selector, scope = document) {
+  return scope.querySelector(selector);
+}
+
+// Get a list of HTML elements as an array
+export function selectAll(selector, scope = document) {
+  return [...scope.querySelectorAll(selector)];
+}
