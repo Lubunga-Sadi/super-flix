ae31ff6d3642cb5aa028b33b634d2627bc5e21a2
Lubunga <158197693+Lubunga-Sadi@users.noreply.github.com>
2024-03-07 10:15:47 -0600

    Update utils.js

--- utils.js
+++ utils.js
@@ -1,16 +1,23 @@
 'use strict';
 
 
-export function getElement(selector, scope = document) {
-  return scope.getElementById(selector);
+export function getElementById(sel, scope = document) {
+  return scope.getElementById(sel);
 }
 
 // Select HTML element
-export function select(selector, scope = document) {
-  return scope.querySelector(selector);
+export function selectEl(sel, scope = document) {
+  return scope.querySelector(sel);
 }
 
 // Get a list of HTML elements as an array
-export function selectAll(selector, scope = document) {
-  return [...scope.querySelectorAll(selector)];
+export function selectAll(sel, scope = document) {
+  return [...scope.querySelectorAll(sel)];
 }
+
+export function print(...args) {
+  args.forEach(element => {
+    console.log(element)
+  });
+}
+               
