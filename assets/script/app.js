f12e5eaa00f566ebd9b4a320ca3768460c808d26
Chase Swedlo <chasechaseswedlo@outlook.com>
2024-03-11 15:42:13 -0500

    Add JavaScript for account dialogue popup

--- assets/script/app.js
+++ assets/script/app.js
@@ -1,1 +1,26 @@
 'use strict';
+
+//Account dialogue functiions
+const loginButton = document.querySelector('.login-button');
+const login = document.querySelector('.log-in');
+const formBox = document.querySelector('.form-box');
+const darkBkg = document.querySelector('.dark-background');
+
+function showCreateAccount() {
+    login.classList.remove("hidden");
+    login.classList.add("shown");
+    formBox.classList.add("translateY");
+    console.log('test');
+}
+
+function hideCreateAccount() {
+    login.classList.remove("shown");
+    login.classList.add("hidden");
+    formBox.classList.remove("translateY");
+}
+
+loginButton.addEventListener("click", showCreateAccount);
+darkBkg.addEventListener("click", hideCreateAccount);
+formBox.addEventListener("click", (event) => {
+    event.stopPropagation();
+});
