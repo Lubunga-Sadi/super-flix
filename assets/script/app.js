453b08281dc07861be521769e7fdeaa3ea5e7238
Lubunga <innocentsadi6@gmail.com>
2024-03-13 09:34:13 -0500

    push check

--- assets/script/app.js
+++ assets/script/app.js
@@ -27,5 +27,59 @@ formBox.addEventListener("click", (event) => {
 
 // SCROLL LANDING-PAGE //
 
+const container = document.querySelector(".scroll");
+const leftButton = document.querySelector(".left");
+const rightButton = document.querySelector(".right");
+const arrows = document.querySelectorAll(".scroll-button");
+let currentIndex = 0;
+const boxes = document.querySelectorAll(".box");
+
+
+function scrollRight() {
+  currentIndex++;
+  if (currentIndex >= boxes.length) {
+    currentIndex = 0;
+  }
+  const nextBox = boxes[currentIndex];
+  container.scroll({
+    left: nextBox.offsetLeft,
+    behavior: "smooth"
+  });
+}
+
+function scrollLeft() {
+  currentIndex--;
+  if (currentIndex < 0) {
+    currentIndex = boxes.length - 1;
+  }
+  const previousBox = boxes[currentIndex];
+  container.scroll({
+    left: previousBox.offsetLeft,
+    behavior: "smooth"
+  });
+}
+rightButton.addEventListener("click", scrollRight);
+leftButton.addEventListener("click", scrollLeft);
 
 // END SCROLL LANDING-PAGE
+
+// SCROLL-LANDING ARROW VISIBILITY 
+function showButtons() {
+    leftButton.classList.add("visible");
+    rightButton.classList.add("visible");
+}
+
+function hideButtons() {
+    leftButton.classList.remove("visible");
+    rightButton.classList.remove("visible");
+}
+
+container.addEventListener('mouseover', showButtons);
+container.addEventListener('mouseout', hideButtons);
+
+leftButton.addEventListener('mouseover', showButtons);
+rightButton.addEventListener('mouseover', showButtons);
+
+leftButton.addEventListener('mouseout', hideButtons);
+rightButton.addEventListener('mouseout', hideButtons);
+// END SCROLL-LANDING ARROW VISIBILITY 
